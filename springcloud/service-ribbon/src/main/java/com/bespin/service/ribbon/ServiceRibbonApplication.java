1de96b0377fdc686fdef88ac9ec7db3bb7ff767b
zhangsun <1873728123@qq.com>
2019-01-15 16:27:51 +0800

    提交负载均衡ribbon+RestTemplate内容

---
+++ springcloud/service-ribbon/src/main/java/com/bespin/service/ribbon/ServiceRibbonApplication.java
@@ -0,0 +1,31 @@
+package com.bespin.service.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    /**
+     * 开启负载均衡功能
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+}
+
